0b3098a2e3ba9cc2348948fdda3b44cacf9169f3
xihadulislam <xihad.bd@gmail.com>
2021-07-03 19:38:45 +0600

    init commit.

--- app/src/main/java/com/project/ucare/MainActivity.java
+++ app/src/main/java/com/project/ucare/MainActivity.java
@@ -1,8 +1,9 @@
 package com.project.ucare;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
