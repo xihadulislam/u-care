14fe4e4df757e52cc64433dd1f57b8f1ece5780a
ShahinMohammad-insaneCoder <shahintahsan225@gmail.com>
2021-07-10 19:35:34 +0600

    emergency

--- app/src/main/java/com/project/ucare/main/MainActivity.java
+++ app/src/main/java/com/project/ucare/main/MainActivity.java
@@ -1,11 +1,17 @@
 package com.project.ucare.main;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.project.ucare.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -24,7 +30,20 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        onCall();
 
+    }
+
+
+    public void onCall() {
+        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    Integer.parseInt("123"));
+        }
     }
 
 }
